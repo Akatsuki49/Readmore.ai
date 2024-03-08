323551876453b512d3bc86ab2099fe59328c0f19
Sowmesh <sowmeshshailesh1234@gmail.com>
2024-03-08 21:37:15 +0530

    Firebase support added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hashcode/firebase_options.dart';
 import 'package:hashcode/views/book_list.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
